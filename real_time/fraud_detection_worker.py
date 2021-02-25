a4be75ba1febb62a52c70f61708ea95165dc4262
Deepyaman Datta <deepyaman.datta@utexas.edu>
2021-02-25 18:39:16 -0500

    Experimentation

--- real_time/fraud_detection_worker.py
+++ real_time/fraud_detection_worker.py
@@ -5,18 +5,24 @@ import json
 
 app = faust.App(
     'fraud_detection_app',
-    broker='kafka://localhost:9092',
+    broker='kafka://localhost:19092',
     value_serializer='raw',
 )
 
-kafka_topic = app.topic('test')
+kafka_topic = app.topic('hello')
 
-@app.agent(kafka_topic)
+@app.agent()
+async def second_process(transactions):
+    async for value in transactions:
+        print('Same input data: ' + str(value))
+
+@app.agent(kafka_topic, sink=[second_process])
 async def process(transactions):
     async for value in transactions:
-        result = requests.post('http://127.0.0.1:5000/invocations', json=json.loads(value))
+        #result = requests.post('http://127.0.0.1:5000/invocations', json=json.loads(value))
         print('Input data: ' + str(value))
-        print('Fraud detection result: ' + str(result.json()))
+        #print('Fraud detection result: ' + str(result.json()))
+        yield json.dumps({"meow": json.loads(value)})
 
 if __name__ == '__main__':    
     # run the consumer
